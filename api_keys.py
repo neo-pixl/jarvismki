646849b04ec599affe556993020e3888b135ab09
neo-pixl <neopixelmn@gmail.com>
2024-07-16 23:25:32 -0500

    Add files via upload

---
+++ api_keys.py
@@ -0,0 +1,13 @@
+SERPAPI_API_KEY = ''
+groq_api = ''
+genai_api = ''
+weather_api = ''
+elevenlabs_api = ''
+
+
+
+# I forgot to mention in the video tutorial. Also get the API key for genai, to get it go to this site:
+
+# https://deepmind.google/technologies/gemini/flash/
+
+# Remember, if you need help let me know.
